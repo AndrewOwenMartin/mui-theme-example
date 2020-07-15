fc5c067918e1f9dde060b7cf553fa8c1c3d96f3e
Andrew Owen Martin <a.martin@gold.ac.uk>
2020-07-15 13:04:50 +0100

    Adding style hook to MyComponent.

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,6 +1,23 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/styles'
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        margin: theme.spacing(3),
+        width: 345,
+    },
+    media: {
+        height: 140,
+    },
+    title: {
+        color: theme.palette.primary.main
+    }
+}))
 
 const MyComponent = () => {
+
+	const classes = useStyles()
+
     return (
         <p>I am a MyComponent!</p>
     )
